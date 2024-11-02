ae5697e917e239c492d5af354292e4496afe7a0d
c2mismo <c2mismo@tuta.io>
2024-11-02 02:37:56 +0100

    Create install_i9.sh

---
+++ install_i9.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Instalamos y preparamos sistema para el procesador i9
+
+pacman -S mesa vulkan-intel
+
+pacman -S thermald
+
+systemctl enable thermald
+
+pacman -S htop intel-gpu-tools
+
+pacman -S cpupower
+
+
+mkdir -p /etc/X11/xorg.conf.d
+cat > /etc/X11/xorg.conf.d/20-intel.conf << EOF
+Section "Device"
+    Identifier  "Intel Graphics"
+    Driver      "intel"
+    Option      "AccelMethod"  "sna"
+    Option      "TearFree"     "true"
+EndSection
+EOF
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=".*"/GRUB_CMDLINE_LINUX_DEFAULT="quiet elevator=bfq"/' /etc/default/grub
+
+echo "Configuracion para i9 terminada."
