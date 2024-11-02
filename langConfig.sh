4c03ae8cdcc50a219f8b05fd4829e50b9df6ad54
c2mismo <c2mismo@tuta.io>
2024-11-02 19:59:58 +0100

    Update langConfig.sh

--- langConfig.sh
+++ langConfig.sh
@@ -14,6 +14,7 @@ locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=es" > /etc/vconsole.conf
+echo "FONT=ter-v32n" >> /etc/vconsole.conf
 
 # Configuración para nuevos usuarios (Español)
 mkdir -p /etc/skel/.config
