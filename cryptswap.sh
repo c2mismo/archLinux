24ad15c4c78ba1fe91d3eb0cc01f2f8b7c391e4f
c2mismo <c2mismo@tuta.io>
2024-11-16 19:59:52 +0100

    Update cryptswap.sh

--- cryptswap.sh
+++ cryptswap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "# /dev/mapper/cryptswap LABEL=cryptswap" | tee -a /etc/fstab
-echo "/dev/mapper/cryptswap none swap sw 0 0" | tee -a /etc/fstab
+echo "# /dev/mapper/cryptswap LABEL=cryptswap" | tee -a /etc/fstab > /dev/null
+echo "/dev/mapper/cryptswap none swap sw 0 0" | tee -a /etc/fstab > /dev/null
 
 [ ! -e /etc/crypttab.initramfs ] && touch /etc/crypttab.initramfs
-echo "# Mount swap re-encrypting it with a fresh key each reboot" | tee -a /etc/crypttab.initramfs
-echo "cryptswap	UUID=YYY /dev/urandom	swap,cipher=aes-xts-plain64,size=256,sector-size=4096" | tee -a /etc/crypttab.initramfs
+echo "# Mount swap re-encrypting it with a fresh key each reboot" | tee -a /etc/crypttab.initramfs > /dev/null
+echo "cryptswap	UUID=YYY /dev/urandom	swap,cipher=aes-xts-plain64,size=256,sector-size=4096" | tee -a /etc/crypttab.initramfs > /dev/null
 
 [ ! -e /etc/sysctl.d/99-sysctl.conf ] && touch /etc/sysctl.d/99-sysctl.conf
-echo "vm.swappiness=10" | tee -a /etc/sysctl.d/99-sysctl.conf
+echo "vm.swappiness=10" | tee -a /etc/sysctl.d/99-sysctl.conf > /dev/null
