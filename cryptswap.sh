c71dd213a00b0bb42c8d044246db821227fb45b6
c2mismo <c2mismo@tuta.io>
2024-11-16 19:25:46 +0100

    Update and rename swapfstab to cryptswap.sh

---
+++ cryptswap.sh
@@ -0,0 +1,9 @@
+echo "# /dev/mapper/cryptswap LABEL=cryptswap" | sudo tee -a /etc/fstab
+echo "/dev/mapper/cryptswap none swap sw 0 0" | sudo tee -a /etc/fstab
+
+[ ! -e /etc/crypttab.initramfs ] && touch /etc/crypttab.initramfs
+echo "# Mount swap re-encrypting it with a fresh key each reboot" | sudo tee -a /etc/crypttab.initramfs
+echo "cryptswap	UUID=YYY /dev/urandom	swap,cipher=aes-xts-plain64,size=256,sector-size=4096" | sudo tee -a /etc/crypttab.initramfs
+
+[ ! -e /etc/sysctl.d/99-sysctl.conf ] && touch /etc/sysctl.d/99-sysctl.conf
+echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
