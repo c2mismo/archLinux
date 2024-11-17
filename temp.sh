da0479ab6fb57b550e8a4af54a673b39287a09db
c2mismo <c2mismo@tuta.io>
2024-11-17 23:53:45 +0100

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -2,6 +2,7 @@
 
 if ! pacman -Qi pacman-contrib > /dev/null 2>&1; then
     echo "Instalando pacman-contrib..."
+    pacman -Sy
     pacman -S --noconfirm pacman-contrib
     else
     echo "pacman-contrib: Previamente instalando..."
