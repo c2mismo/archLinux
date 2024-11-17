8850a490ee5f495c6f3771097480d10230c9e112
c2mismo <c2mismo@tuta.io>
2024-11-17 17:45:48 +0100

    Update and rename disable-sleep.conf to disable-sleep.sh

---
+++ disable-sleep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Desabilitamos la hibernación,
+# obligatorio si queremos tener una
+# swap volatil
+
+# Asegúrate de que el script se ejecute como root
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, ejecuta este script como root."
+  exit 1
+fi
+
+cat > /etc/systemd/sleep.conf.d << EOF
+[Sleep]
+AllowSuspend=no
+AllowHibernation=no
+AllowHybridSleep=no
+AllowSuspendThenHibernate=no
+EOF
+
