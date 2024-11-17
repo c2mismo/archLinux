ced619248487c82571566cdedfc8659ae7e8404d
c2mismo <c2mismo@tuta.io>
2024-11-17 23:58:11 +0100

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if ! pacman -Qi pacman-contrib > /dev/null 2>&1; then
-    echo "Instalando pacman-contrib..."
-    pacman -Sy
-    pacman -S --noconfirm pacman-contrib
-    else
-    echo "pacman-contrib: Previamente instalando..."
-fi
+# Crear hook para paccache
+HOOK_FILE="/etc/pacman.d/hooks/clean_package_cache.hook"
+
+echo "Creando hook para paccache..."
+
+mkdir -p /etc/pacman.d/hooks
+
+cat > "$HOOK_FILE" << EOF
+[Trigger]
+Operation = Upgrade
+Operation = Install
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+Description = Limpiando caché de pacman...
+When = PostTransaction
+Exec = /usr/bin/paccache -rk1
+EOF
+
+echo "Hook de paccache creado."
 rm temp.sh
