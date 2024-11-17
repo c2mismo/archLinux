9f60041c31b038b74899d603bab1515f878f7ff3
c2mismo <c2mismo@tuta.io>
2024-11-17 11:54:20 +0100

    Update cryptroot.sh

--- cryptroot.sh
+++ cryptroot.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
+# Partición de root
+ROOT_PART="/dev/nvme0n1p5"
 
 # Ruta del archivo a modificar
 CRYPTTAB_FILE="/etc/crypttab.initramfs"
-
-
-# Verificamos que existe crypttab.initramfs y si no lo creamos y le añadimos las lineas necesarias
-# para que encripte la partición al cada inicio
-[ ! -e $CRYPTTAB_FILE ] && cp /etc/crypttab $CRYPTTAB_FILE
-echo "" | tee -a $CRYPTTAB_FILEE > /dev/null
-echo "# Mount root as /dev/mapper/cryptroot using LUKS, and prompt for the passphrase at boot time." | tee -a $CRYPTTAB_FILEE > /dev/null
-echo "cryptroot    UUID=XXX    none    luks,discard,no-read-workqueue,no-write-workqueue,password-echo=no" | tee -a $CRYPTTAB_FILE > /dev/null
-
 
 # Contador de intentos
 attempts=0
@@ -20,7 +13,7 @@ uuid=""
 
 # Verificar el UUID hasta 3 veces
 while [ $attempts -lt $max_attempts ]; do
-    uuid=$(blkid -s UUID -o value /dev/nvme0n1p5)
+    uuid=$(blkid -s UUID -o value $ROOT_PART)
     
     if [ -n "$uuid" ]; then
         echo "UUID encontrado: $uuid"
@@ -33,11 +26,14 @@ while [ $attempts -lt $max_attempts ]; do
     fi
 done
 
-# Si se encontró un UUID, modificar el archivo
+# Si se encontró un UUID
+# Verificamos que existe crypttab.initramfs y si no lo creamos y le añadimos las lineas necesarias
+# para que encripte la partición al cada inicio
 if [ -n "$uuid" ]; then
-    echo "Modificando el archivo $CRYPTTAB_FILE..."
-    sed -i "s/XXX/$uuid/g" "$CRYPTTAB_FILE"
-    echo "Se ha reemplazado 'XXX' por '$uuid' en $CRYPTTAB_FILE."
+    [ ! -e $CRYPTTAB_FILE ] && cp /etc/crypttab $CRYPTTAB_FILE
+    echo "" | tee -a $CRYPTTAB_FILEE > /dev/null
+    echo "# Mount root as /dev/mapper/cryptroot using LUKS, and prompt for the passphrase at boot time." | tee -a $CRYPTTAB_FILEE > /dev/null
+    echo "cryptroot    UUID=$uuid    none    luks,discard,no-read-workqueue,no-write-workqueue,password-echo=no" | tee -a $CRYPTTAB_FILE > /dev/null
 else
     echo "No se pudo encontrar un UUID después de $max_attempts intentos."
 fi
