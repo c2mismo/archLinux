be8d246ea5ca3def8b235aaba3cac9d250f0e639
c2mismo <c2mismo@tuta.io>
2024-11-17 23:51:31 +0100

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,46 +1,9 @@
 #!/bin/bash
 
-
-echo "Configurando pacman..."
-
-# Ruta al archivo de configuración de pacman
-PACMAN_CONF="/etc/pacman.conf"
-
-# Función para descomentar una opción con dos líneas
-update_multilib() {
-    local section="$1"
-    local include_line="$2"
-    local pacman_conf="$PACMAN_CONF"
-
-    # Verificar si la sección [multilib] existe
-    if grep -q "$section" "$pacman_conf"; then
-        echo "Encontrado $section en $pacman_conf"
-
-        # Obtener el número de línea de la sección [multilib]
-        local line_number=$(grep -n "$section" "$pacman_conf" | cut -d: -f1)
-
-        # Modificar la línea de la sección [multilib]
-        sed -i "${line_number}s|^#||" "$pacman_conf"  # Eliminar el # de la sección
-        echo "Descomentado $section en $pacman_conf"
-
-        # Verificar si la línea siguiente existe y contiene el valor de $include_line
-        if [ "$((line_number + 1))" -le "$(wc -l < "$pacman_conf")" ]; then
-            local next_line=$(sed -n "$((line_number + 1))p" "$pacman_conf")
-            if [[ "$next_line" == *"$include_line"* ]]; then
-                sed -i "$((line_number + 1))s|^#||" "$pacman_conf"  # Eliminar el # de Include
-                echo "Descomentado $include_line en $pacman_conf"
-            else
-                echo "ERROR: La línea siguiente a $section no contiene el valor esperado: $include_line"
-            fi
-        else
-            echo "ERROR: No se puede modificar la línea siguiente a $section porque no existe."
-        fi
+if ! pacman -Qi pacman-contrib > /dev/null 2>&1; then
+    echo "Instalando pacman-contrib..."
+    pacman -S --noconfirm pacman-contrib
     else
-        echo "ERROR: No se ha encontrado $section en $pacman_conf"
-    fi
-}
-
-# Habilitar repositorio multilib
-update_multilib "#\[multilib\]" "#Include = /etc/pacman.d/mirrorlist"
-
+    echo "pacman-contrib: Previamente instalando..."
+fi
 rm temp.sh
