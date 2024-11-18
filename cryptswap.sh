0562bbdb743486d4d4687a920b106c16aee152b5
c2mismo <c2mismo@tuta.io>
2024-11-18 02:55:02 +0100

    Update cryptswap.sh

--- cryptswap.sh
+++ cryptswap.sh
@@ -6,9 +6,9 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Configuramos el sistema para que haga el menor uso de swap posible
 [ ! -e /etc/sysctl.d/99-sysctl.conf ] && touch /etc/sysctl.d/99-sysctl.conf
 echo "vm.swappiness=10" | tee -a /etc/sysctl.d/99-sysctl.conf > /dev/null
+echo "Configurado el sistema para que haga el menor uso de swap posible."
 
 # Partición swap
 SWAP_PART="/dev/nvme0n1p8"
@@ -16,7 +16,7 @@ SWAP_PART="/dev/nvme0n1p8"
 SWAP_LABEL="swap"
 # NAME desencriptada
 SWAP_CRYPT="volatileswap"
-# Ruta del archivo a modificar
+# Ruta de los archivos a modificar
 CRYPTTAB_FILE="/etc/crypttab.initramfs"
 FSTAB_FILE="/etc/fstab"
 
@@ -27,7 +27,9 @@ FSTAB_FILE="/etc/fstab"
 # Esto significa que el UUID y el LABEL de la partición de swap no se sobrescriben,
 # ya que no se modifican los primeros sectores de la partición donde se almacenan estos identificadores.
 
-mkfs.ext4 -L $SWAP_LABEL -m 0 -O ^has_journal $SWAP_PART -s 1M
+mkfs.ext4 -L $SWAP_LABEL -m 0 -O ^has_journal $SWAP_PART -s 1M && \
+echo "Creado pequeña particion para el offset de proteccion del UUID y el LABEL." || \
+echo "ERROR: Creando pequeña particion para el offset de proteccion del UUID y el LABEL."
 
 # -m 0: No reserva espacio para el superusuario, maximizando el espacio disponible para swap.
 # -O ^has_journal: Desactiva el journaling, lo que es innecesario para una partición de swap y mejora el rendimiento.
@@ -43,7 +45,11 @@ cat >> "$CRYPTTAB_FILE" << EOF
 # Especificamos un offset=2048 para que el UUID y el LABEL de la partición de swap no se sobrescriban.
 $SWAP_CRYPT    LABEL=$SWAP_LABEL    /dev/urandom    swap,offset=2048,cipher=aes-xts-plain64,size=256,sector-size=4096
 EOF
-
+if grep -q "$SWAP_CRYPT" "$CRYPTTAB_FILE"; then
+echo "Añadido al "$CRYPTTAB_FILE" para generar una swap volatil."
+else
+echo "ERROR: Al añadirlo al "$CRYPTTAB_FILE" para generar una swap volatil."
+fi
 
 
 
@@ -53,4 +59,10 @@ cat >> "$FSTAB_FILE" << EOF
 /dev/mapper/$SWAP_CRYPT    none    swap    sw    0 0
 
 EOF
+
+if grep -q "$SWAP_CRYPT" "$FSTAB_FILE"; then
+echo "Añadido al "$FSTAB_FILE" para montar la swap volatil."
+else
+echo "ERROR: Al añadirlo al "$FSTAB_FILE" para montar la swap volatil."
+fi
 
