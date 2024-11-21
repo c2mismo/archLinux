e102db73e038b01791ca969397f6443bb6650163
c2mismo <c2mismo@tuta.io>
2024-11-21 16:39:16 +0100

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
 # Hacemos una copia de seguridad
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-# descargamos una lista completa de los servidores oficiales de arch
-if [ -e /etc/pacman.d/mirrorlist_all ]; then
-  cp /etc/pacman.d/mirrorlist_all /etc/pacman.d/mirrorlist_all.backup
-fi
-curl -o /etc/pacman.d/mirrorlist_all https://archlinux.org/mirrorlist/all/
-
-# Podriamos filtrar los servidores solo para los de españa
-if [ -e /etc/pacman.d/mirrorlist_es ]; then
-  cp /etc/pacman.d/mirrorlist_es /etc/pacman.d/mirrorlist_es.backup
-fi
-sed -n '/## Spain/,/^$/p' /etc/pacman.d/mirrorlist_all > /etc/pacman.d/mirrorlist_es
-
-
+# Obtener la lista de servidores desde el sitio oficial
+sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/all/
 
 # Ordenar los Espejos por Velocidad: 
-# Arch Linux proporciona una herramienta llamada
-# rankmirrors que puede ayudarte a clasificar los espejos por velocidad.
-# es uno de los paquetes del contenedor pacman-contrib
+# Arch Linux proporciona una herramienta llamada reflector que puede ayudarte a clasificar los espejos por velocidad.
 
-if ! pacman -Qi pacman-contrib > /dev/null 2>&1; then
-    echo "Instalando pacman-contrib..."
-    pacman -Sy
-    pacman -S --noconfirm pacman-contrib
-    else
-    echo "pacman-contrib: Previamente instalando..."
+# Verificar si reflector está instalado
+if ! pacman -Qi reflector > /dev/null 2>&1; then
+    echo "Instalando reflector..."
+    sudo pacman -Sy --noconfirm reflector
+else
+    echo "reflector: Previamente instalado."
 fi
 
-# Aquí, -n 6 indica que deseas mantener los 6 espejos más rápido
-rankmirrors -n 6 /etc/pacman.d/mirrorlist_all | sudo tee /etc/pacman.d/mirrorlist
+# Aquí, -n 6 indica que deseas mantener los 6 espejos más rápidos
+sudo reflector --latest 6 --sort rate --save /etc/pacman.d/mirrorlist
 
-# seguramente si es eficiente el programa usemos del all el mas rapido.
+# Sincronizar la base de datos de pacman
+sudo pacman -Syy
