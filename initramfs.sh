0b2c8c3d7e059071165dc6d3f7e05863c7d1a5cb
c2mismo <c2mismo@tuta.io>
2024-11-21 14:59:28 +0100

    Update initramfs.sh

--- initramfs.sh
+++ initramfs.sh
@@ -7,48 +7,36 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Ruta al archivo de configuración
-MKINITCPIO_CONF="/etc/mkinitcpio.conf.backup"
+MKINITCPIO_CONF="/etc/mkinitcpio.conf"
+VCONSOLE_CONF="/etc/vconsole.conf"
+BACKUP_CONF="/etc/mkinitcpio.conf.backup"
 
-cp MKINITCPIO_CONF MKINITCPIO_CONF.backup
+# Hacer una copia de seguridad del archivo de configuración
+cp "$MKINITCPIO_CONF" "$BACKUP_CONF"
 
-# Modificar la línea de MODULES para porcesadores Intel
+# Modificar la línea de MODULES para procesadores Intel
 sed -i 's/^MODULES=().*/MODULES=(i915)/' "$MKINITCPIO_CONF"
 
-# Incluimos los modules del kernel necesarios para que initramfs pueda desencriptar
-# usando systemd, recuerda sd-encrypt debe estar antes que filesystems
-# Comentamos la linea para udev porque vamos a usar systemd
-# Incluimos los modules del kernel necesarios para que initramfs pueda desencriptar
-# usando systemd, recuerda sd-encrypt debe estar antes que filesystems y
-# Primero verificamos si existe el archivo /etc/vconsole.conf y si contiene 'FONT='
-# para añadir el HOOK consolefont al usar systemd sería sd-vconsole
+# Inicializar la variable flag
+flag=0
 
+# Verificar si existe el archivo /etc/vconsole.conf y si contiene 'FONT='
 if [ -f "$VCONSOLE_CONF" ]; then
-
   if grep -q '^FONT=' "$VCONSOLE_CONF"; then
-
     # Modificar la línea de HOOKS si FONT= está presente
-
     sed -i 's/^HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block filesystems fsck)/#    HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block filesystems fsck)/' "$MKINITCPIO_CONF"
-
     sed -i 's/^#    HOOKS=(base systemd autodetect modconf kms keyboard sd-vconsole sd-encrypt block filesystems fsck)/HOOKS=(base systemd autodetect microcode modconf kms keyboard keymap sd-vconsole sd-encrypt block filesystems fsck)/' "$MKINITCPIO_CONF"
-
-    echo "La línea de HOOKS ha sido modificada en $MKINITCPIO_CONF." && flag=1;
-
+    echo "La línea de HOOKS ha sido modificada en $MKINITCPIO_CONF."
+    flag=1
   else
-
     echo "Advertencia: El archivo $VCONSOLE_CONF no contiene 'FONT='. Por favor, modifícalo antes de continuar."
-
   fi
-
 else
-
   echo "Advertencia: El archivo $VCONSOLE_CONF no existe. Por favor, crea este archivo antes de continuar."
-
 fi
 
-
-if [ $flag = "1" ]; then
-  # Confirmar que se han realizado los cambios
+# Confirmar que se han realizado los cambios
+if [ $flag -eq 1 ]; then
   echo "Modificaciones realizadas en $MKINITCPIO_CONF:"
   grep '^MODULES=' "$MKINITCPIO_CONF"
   grep '^HOOKS=' "$MKINITCPIO_CONF"
@@ -58,4 +46,3 @@ if [ $flag = "1" ]; then
   
   echo "La imagen del initramfs ha sido regenerada."
 fi
-
