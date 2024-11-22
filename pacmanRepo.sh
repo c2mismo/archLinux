771b055ea60257aaf898f734183fc341548dd343
c2mismo <c2mismo@tuta.io>
2024-11-22 17:49:14 +0100

    Update pacmanRepo.sh

--- pacmanRepo.sh
+++ pacmanRepo.sh
@@ -14,7 +14,7 @@ fi
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
 # Obtener la lista de servidores desde el sitio oficial
-sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/all/
+# sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/all/
 
 # Ordenar los Espejos por Velocidad: 
 # Arch Linux proporciona una herramienta llamada reflector que puede ayudarte a clasificar los espejos por velocidad.
@@ -25,4 +25,4 @@ sudo reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 # Sincronizar la base de datos de pacman
 sudo pacman -Syy
 
-rm temp.sh
+rm pacmanRepo.sh
