ed6e2e2d036653170a2092d9a39a5c04e691ebbc
c2mismo <c2mismo@tuta.io>
2024-11-27 16:09:08 +0100

    Update initinstall.sh

--- initinstall.sh
+++ initinstall.sh
@@ -3,4 +3,5 @@
 # Instalamos el sistema base en /mnt
 pacstrap -i /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode nano less git btrfs-progs exfat-utils ntfs-3g grub efibootmgr networkmanager wget
 
-rm initinstall.sh
+# Limpiar los archivos temporales
+sudo rm -f "$0"
