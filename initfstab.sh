692ed9e35c67e8238938ed50a84b6ebd211c949d
c2mismo <c2mismo@tuta.io>
2025-01-05 16:51:15 +0100

    Update initfstab.sh

--- initfstab.sh
+++ initfstab.sh
@@ -2,9 +2,16 @@
 
 # Creamos el fstab para el montaje automático al inicio del sistema
 
-genfstab -U /mnt >> /mnt/etc/fstab && echo "fstab creado"
+# genfstab -U /mnt >> /mnt/etc/fstab && echo "fstab creado"
 
-cat /mnt/etc/fstab
+# cat /mnt/etc/fstab
+
+# Creamos el fstab para el montaje automático al inicio del sistema
+# desde el sistema despues de haber creado el user
+
+genfstab -U -p / >> /etc/fstab
+
+cat /etc/fstab
 
 # Limpiar los archivos temporales
 sudo rm -f "$0"
