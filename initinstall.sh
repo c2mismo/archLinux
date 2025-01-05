f29b0fe98f8c39d87a3b6f14e8cb392351061636
c2mismo <c2mismo@tuta.io>
2025-01-05 20:08:57 +0100

    Update initinstall.sh

--- initinstall.sh
+++ initinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instalamos el sistema base en /mnt
-pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware intel-ucode usbutils nano less git btrfs-progs exfat-utils ntfs-3g grub efibootmgr networkmanager wget
+pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware intel-ucode usbutils nano less git btrfs-progs exfat-utils ntfs-3g xfsprogs grub efibootmgr networkmanager wget
 
 # Limpiar los archivos temporales
 sudo rm -f "$0"
