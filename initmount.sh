5ccef3b0d58dd8717dd434bab618f2c42ff26a5a
c2mismo <c2mismo@tuta.io>
2025-04-29 17:49:41 +0200

    Update initmount.sh

--- initmount.sh
+++ initmount.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Montamos las particiones iniciales
 
-mount /dev/mapper/cryptroot /mnt && echo "cryptroot montado"
+mount /dev/mapper/cryptarch /mnt && echo "cryptroot montado"
 
 mkdir /mnt/boot && echo "Directorio /boot creado"
-mount /dev/nvme0n1p4 /mnt/boot && echo "boot montado"
+mount /dev/nvme0n1p5 /mnt/boot && echo "boot montado"
 
 mkdir /mnt/boot/efi && echo "Directorio /boot/efi creado"
 mount /dev/nvme0n1p1 /mnt/boot/efi && echo "efi montado"
 
-mkdir /mnt/srv && mkdir /mnt/srv/samba && echo "Directorio /srv/samba creado"
-mount /dev/nvme0n1p6 /mnt/srv/samba && echo "samba montado"
+# mkdir /mnt/srv && mkdir /mnt/srv/samba && echo "Directorio /srv/samba creado"
+# mount /dev/nvme0n1p6 /mnt/srv/samba && echo "samba montado"
 
 # Limpiar los archivos temporales
 sudo rm -f "$0"
