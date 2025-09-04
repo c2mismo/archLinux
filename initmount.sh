a6e4b001f173917f813f60db2234ecac749925fc
c2mismo <c2mismo@tuta.io>
2025-09-04 19:53:22 +0200

    Update initmount.sh

--- initmount.sh
+++ initmount.sh
@@ -9,12 +9,11 @@ mount /dev/nvme0n1p5 /mnt/boot && echo "boot montado"
 mkdir /mnt/boot/efi && echo "Directorio /boot/efi creado"
 mount /dev/nvme0n1p1 /mnt/boot/efi && echo "efi montado"
 
-# mkdir /mnt/srv && mkdir /mnt/srv/samba && echo "Directorio /srv/samba creado"
-# mount /dev/nvme0n1p6 /mnt/srv/samba && echo "samba montado"
-
+mkdir /mnt/mnt && mkdir /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows creado"
+mount /dev/nvme0n1p6 /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows montado"
 
 mkdir /mnt/mnt/Win11 && echo "Directorio /mnt/Win11 creado"
-mount /dev/nvme0n1p3 /mnt/mnt/Win11 && echo "Win11 montado"
+mount /dev/nvme0n1p3 /mnt/mnt/Win11 && echo "Win11 montado, cuidado cualquier cambio puede dañar Windows"
 
 # Limpiar los archivos temporales
 sudo rm -f "$0"
