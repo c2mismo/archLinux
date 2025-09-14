40224c9ec2ae56e5a125ade9484f36e91d6bc2f2
c2mismo <c2mismo@tuta.io>
2025-09-14 18:15:54 +0200

    Delete mountWinLinux.sh

--- mountWinLinux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Montamos la particion compartida con windows
-
-mkdir /run/media/c2mismo && mkdir /run/media/c2mismo/WinLinux && echo "Directorio /run/media/c2mismo/WinLinux creado" || echo "ERROR: Directorio /run/media/c2mismo/WinLinux creado."
-mount /dev/nvme0n1p6 /run/media/c2mismo/WinLinux && echo "WinLinux montado" || echo "ERROR: WinLinux no montado."
-
-# Limpiar los archivos temporales
-sudo rm -f "$0"
